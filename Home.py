ce0cc9fd9a80c7dda37377999f2877249687fa76
Panuwat <644230052@webmail.npru.ac.th>
2023-12-15 14:30:20 +0700

    .commit

--- Home.py
+++ Home.py
@@ -7,8 +7,8 @@ st.subheader('สาขาวิชาเทคโนโลยีสารสน
 st.markdown("----")
 
 col1, col2 = st.columns(2)
-col1.write("This is column 1")
-col2.write("This is column 2")
+#col1.write("This is column 1")
+#col2.write("This is column 2")
 with col1:
     st.image('./pic/pf.jpg')
 with col2:

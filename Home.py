df7f6e2ce1588856dc9ac0b71e4a8f4e42598a6a
Panuwat <644230052@webmail.npru.ac.th>
2023-12-15 15:50:04 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
+
 st.header("Panuwat Sittirat")
+st.balloons()
 st.title('การทดสอบเขียนเว็บด้วย Python')
 st.header('นาย ภานุวัฒน์ สิทธิรัตน์')
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')

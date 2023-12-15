30a958911de3629c0693d5bf756633f97a1d6512
Panuwat <644230052@webmail.npru.ac.th>
2023-12-15 14:31:34 +0700

    .commit

--- Home.py
+++ Home.py
@@ -10,6 +10,6 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./pic/pf.jpg')
+    st.image('./p/pf.jpg')
 with col2:
-    st.image('./pic/pg.jpg')
+    st.image('./p/pg.jpg')
